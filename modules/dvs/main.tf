125880d11aef58a21a3d0ea46332b583f3cf9e93
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-02-26 11:26:12 +0100

    fix-dvs

--- modules/dvs/main.tf
+++ modules/dvs/main.tf
@@ -10,6 +10,7 @@ resource "nomad_job" "dvs" {
       dc_names            = var.dc_names
       nameserver_dummy_ip = var.nameserver_dummy_ip
       jobs_constraint     = var.jobs_constraint
+      cp_jobs_constraint  = var.cp_jobs_constraint
       dvs_ws_url          = var.dvs_ws_url
       dvs_http_url        = var.dvs_http_url
       dvs_google_api_key  = var.dvs_google_api_key

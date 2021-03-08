e35d8690b0b32ddced58e064abb567ee65d5d0fa
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-03-08 11:15:54 +0100

    fixes

--- modules/dvs/versions.tf
+++ modules/dvs/versions.tf
@@ -1,13 +1,7 @@
 terraform {
   required_providers {
-    consul = {
-      source = "hashicorp/consul"
-    }
     nomad = {
       source = "hashicorp/nomad"
-    }
-    vault = {
-      source = "hashicorp/vault"
     }
   }
   required_version = "~> 0.13"

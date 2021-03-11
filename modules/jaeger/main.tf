be06a23772bd24cfda38b50a614abc150cd5f788
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-03-11 12:36:10 +0100

    fix: syntax

--- modules/jaeger/main.tf
+++ modules/jaeger/main.tf
@@ -7,12 +7,12 @@ resource "nomad_job" "jaeger_jobs" {
   jobspec = templatefile(
     "${path.module}/${each.value}",
     {
-      dc_names                  = var.dc_names,
-      services_domain           = var.services_domain,
-      jaeger_jobs_constraints   = var.jaeger_jobs_constraints,
-      elastic_service_name      = var.elastic_service_name,
-      jeager_agent_service_name = var.jeager_agent_service_name,
-      artifacts_source_prefix   = var.artifacts_source_prefix,
+      dc_names                                 = var.dc_names,
+      services_domain                          = var.services_domain,
+      jaeger_jobs_constraints                  = var.jaeger_jobs_constraints,
+      elastic_service_name                     = var.elastic_service_name,
+      jaeger_agent_service_name                = var.jaeger_agent_service_name,
+      artifact_source_jaeger_spark_dependecies = var.artifact_source_jaeger_spark_dependecies,
     }
   )
 }

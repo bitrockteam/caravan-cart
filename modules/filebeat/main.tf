ab730d1003e3187ac8162146770fa5af2d3f4f2c
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-03-11 12:47:48 +0100

    fix: filebeat module

--- modules/filebeat/main.tf
+++ modules/filebeat/main.tf
@@ -2,10 +2,11 @@ resource "nomad_job" "filebeat" {
   jobspec = templatefile(
     "${path.module}/filebeat_job.hcl",
     {
-      dc_names                  = var.dc_names,
-      services_domain           = var.services_domain,
-      elastic_service_name      = var.elastic_service_name,
-      logstash_jobs_constraints = var.logstash_jobs_constraints
+      dc_names             = var.dc_names,
+      domain               = var.domain
+      services_domain      = var.services_domain,
+      elastic_service_name = var.elastic_service_name,
+      kibana_service_name  = var.kibana_service_name
     }
   )
 }
